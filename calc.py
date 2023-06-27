62c0daf0a1d13272fbd77182025d3a10a77189f5
risharaceer <137887629+risharaceer@users.noreply.github.com>
2023-06-27 13:28:02 +0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,42 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        print("Ошибка: деление на ноль")
+        return None
+
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
+
+choice = input("Введите номер операции (1/2/3/4): ")
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+result = None
+
+if choice == '1':
+    result = add(num1, num2)
+elif choice == '2':
+    result = subtract(num1, num2)
+elif choice == '3':
+    result = multiply(num1, num2)
+elif choice == '4':
+    result = divide(num1, num2)
+else:
+    print("Неправильный ввод")
+
+if result is not None:
+    print("Результат:", result)
